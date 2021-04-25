b21f3c68a677023973b39c6e8e575897ca30fce2
Ben Gianni <bgianni576@gmail.com>
2021-04-25 10:52:29 -0400

    working example

--- src/main/java/gianni/benjamin/client/Listener.java
+++ src/main/java/gianni/benjamin/client/Listener.java
@@ -1,4 +1,54 @@
 package gianni.benjamin.client;
 
+import java.util.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
 public class Listener {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please specify which hashtag you would like to listen for: ");
+        String hashtagListener = scan.nextLine();
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,"TwitterKafka");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(hashtagListener));
+
+        final int giveUp = 100;   int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords =
+                    consumer.poll(1000);
+
+            if (consumerRecords.count()==0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp) {
+                    break;
+                }
+                else {
+                    continue;
+                }
+            }
+
+            consumerRecords.forEach(record -> {
+                System.out.printf("Consumer Record:(%s, %d)\n",
+                        record.value(),
+                        record.offset());
+            });
+
+            consumer.commitAsync();
+        }
+        consumer.close();
+        System.out.println("DONE");
+    }
 }
